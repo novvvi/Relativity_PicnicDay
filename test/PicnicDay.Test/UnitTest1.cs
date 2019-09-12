9979028f9eee0b4bef524ca2b3ade7c86e61fa1b
novvvi <novehui@gmail.com>
2019-09-12 17:09:43 -0500

    remove function in unittest

--- test/PicnicDay.Test/UnitTest1.cs
+++ test/PicnicDay.Test/UnitTest1.cs
@@ -1,18 +1,16 @@
 using System;
 using Xunit;
-using PicnicDay.Controllers;
-using PicnicDay.Models;
+
 
 namespace PicnicDay.Test
 {
     public class UnitTest1
     {
-        AirportsController controller = new AirportsController();
+
         [Fact]
         public void ReturnResultModel()
         {
-            // var returnResultModel = controller.result("KORD");
-            // Assert.Equals()
+            
         }
     }
 }
